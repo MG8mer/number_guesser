4d70ab7e3586a76d743026fb1cc69000569dcef8
MG8mer <mhammouri.26@acsamman.edu.jo>
2023-07-03 22:04:44 +0300

    Addd more user interaction with the game.

--- script.py
+++ script.py
@@ -2,18 +2,23 @@ import random
 
 print("Welcome to this Number Guessing Game!")
 print("")
-print("Your objective is to guess a randomly generated number from 1 to 100!")
-print("The caveat, however, is that you must do this within 10 attempts!")
+print("In this number guessing game, you get to determine the range of numbers you want your run to have!")
+print("You also are able to alter the difficulty by determining the amount of attempts you have to guess said number!")
 print("")
-print("Can you read the computer's mind and succeed this challenge? We shall find out...")
+print("Will you be able to read the computer's mind and succeed this challenge? We shall find out...")
 print("")
 
 def number_guess():
     guess = None
     attempts = 0
+    invalid_attempts = 0
+
+    x = None
+    y = None
+    z = None
 
     while True:
-        if attempts == 10:
+        if attempts == z:
             print("Game over! You've exhausted your attempts.")
             print("")
             print(f"The number was {rng_game}.")
@@ -21,37 +26,104 @@ def number_guess():
             print("")
             if response == 'Y' or response == 'y':
                 attempts = 0
+                invalid_attempts = 0
+                x = None
+                y = None
+                z = None
             else:
                 break
-        if attempts == 0:
-            rng_game = random.randint(1, 100)
+        if x == None and y == None and z == None:
+            while True:
+                while True:
+                    try:
+                        x = int(input("Input the minimum value of your range: "))
+                    except ValueError:
+                        print("That wasn't an integer... Please try again.")
+                        print("")
+                    try:
+                        y = int(input("Input the maximum value of your range: "))
+                    except ValueError:
+                        print("That wasn't an integer... Please try again.")
+                        print("")
+                    while True:
+                        if x > y:
+                            print("")
+                            print("Your minimum value is larger than your maximum... Try again.")
+                            print("")
+                            break
+                        if x == y:
+                            print("")
+                            print("Your minimum value is equal to your maximum... Try again.")
+                            print("")
+                            break
+                        elif x < y:
+                            break
+                    if x < y:
+                        break
+                while True:
+                    try:
+                        z = int(input("Input the amount of attempts you would like to have: "))
+                    except ValueError:
+                        print("That wasn't an integer... Please try again.")
+                        print("")
+                    while True:
+                        if z <= 0:
+                            print("")
+                            print("You can't have less than 1 attempt... Try again.")
+                            print("")
+                            break
+                        elif z > 0:
+                            break
+                    if z > 0:
+                        break
+                while True:
+                    print("")
+                    print(f"Scenario: You're guessing a random number from {x} to {y} and you have {z} attempt(s) to do so.")
+                    print("")
+                    redo = input("Would you like to reset your scenario? Y/N: ")
+                    if redo == "N" or redo == "n":
+                        print("")
+                        print("Good luck!")
+                        print("")
+                        break
+                    else:
+                        print("")
+                        break
+                if redo == "N" or redo == "n":
+                    break
+
+        if attempts == 0 and invalid_attempts == 0:
+            rng_game = random.randint(x, y)
         while guess is None:
                 try:
                     guess = int(input("Guess the number: "))
                 except ValueError:
-                    print(f"That wasn't an integer... Your guess must be an integer between 1 and 100. Don't worry, you still have {10 - attempts} attempts remaining.")
+                    invalid_attempts += 1
+                    print(f"That wasn't an integer... Your guess must be an integer between 1 and 100. Don't worry, you still have {z - attempts} attempts remaining.")
                     print("")
-        if attempts == 10 and response != 'Y' and response != 'y':
+        if attempts == z and response != 'Y' and response != 'y':
             break
 
         if guess > rng_game:
-            if guess > 100:
-                print(f"That guess is higher than 100!... Remember, your guess must be between 1 and 100. Don't worry, you still have {10 - attempts} attempts remaining.")
+            if guess > y:
+                invalid_attempts += 1
+                print(f"That guess is higher than {y}!... Remember, your guess must be between {x} and {y}. Don't worry, you still have {z - attempts} attempts remaining.")
                 print("")
                 guess = None
             else:
                 attempts += 1
-                print(f"Your guess was too high... You have {10 - attempts} attempts remaining.")
+                print(f"Your guess was too high... You have {z - attempts} attempts remaining.")
                 print("")
                 guess = None
         elif guess < rng_game:
-            if guess < 1:
-                print(f"That guess is lower than 1!... Remember, your guess must be between 1 and 100. Don't worry, you still have {10 - attempts} attempts remaining.")
+            if guess < x:
+                invalid_attempts += 1
+                print(f"That guess is lower than {x}!... Remember, your guess must be between {x} and {y}. Don't worry, you still have {z - attempts} attempts remaining.")
                 print("")
                 guess = None
             else:
                 attempts += 1
-                print(f"Your guess was too low... You have {10 - attempts} attempts remaining.")
+                print(f"Your guess was too low... You have {z - attempts} attempts remaining.")
                 print("")
                 guess = None
         elif guess == rng_game:
@@ -60,9 +132,12 @@ def number_guess():
             guess = None
             response = input("Wanna do it again? Y/N: ")
             print("")
+            invalid_attempts = 0
+            x = None
+            y = None
+            z = None
             if response == 'Y' or response == 'y':
                 attempts = 0
             else:
                 break
-
 number_guess()
