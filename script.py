8ad27fad986837722a2065e3d0abf5087580c383
F. Eber <gitfeber@proton.me>
2023-07-04 14:41:11 +0200

    split game into multiple functions - more readable/smaller script

--- script.py
+++ script.py
@@ -1,143 +1,73 @@
 import random
 
-print("Welcome to this Number Guessing Game!")
-print("")
-print("In this number guessing game, you get to determine the range of numbers you want your run to have!")
-print("You also are able to alter the difficulty by determining the amount of attempts you have to guess said number!")
-print("")
-print("Will you be able to read the computer's mind and succeed this challenge? We shall find out...")
-print("")
-
 def number_guess():
-    guess = None
-    attempts = 0
-    invalid_attempts = 0
-
-    x = None
-    y = None
-    z = None
+    print("Welcome to this Number Guessing Game!")
+    print("In this number guessing game, you get to determine the range of numbers you want to play with!")
+    print("You can also adjust the difficulty by choosing the number of attempts you have to guess the number.")
+    print("Let's see if you can guess the computer's number!")
 
     while True:
-        if attempts == z:
-            print("Game over! You've exhausted your attempts.")
-            print("")
-            print(f"The number was {rng_game}.")
-            response = input("Wanna do it again? Y/N: ")
-            print("")
-            if response == 'Y' or response == 'y':
-                attempts = 0
-                invalid_attempts = 0
-                x = None
-                y = None
-                z = None
-            else:
-                break
-        if x == None and y == None and z == None:
-            while True:
-                while True:
-                    try:
-                        x = int(input("Input the minimum value of your range: "))
-                    except ValueError:
-                        print("That wasn't an integer... Please try again.")
-                        print("")
-                    try:
-                        y = int(input("Input the maximum value of your range: "))
-                    except ValueError:
-                        print("That wasn't an integer... Please try again.")
-                        print("")
-                    while True:
-                        if x > y:
-                            print("")
-                            print("Your minimum value is larger than your maximum... Try again.")
-                            print("")
-                            break
-                        if x == y:
-                            print("")
-                            print("Your minimum value is equal to your maximum... Try again.")
-                            print("")
-                            break
-                        elif x < y:
-                            break
-                    if x < y:
-                        break
-                while True:
-                    try:
-                        z = int(input("Input the amount of attempts you would like to have: "))
-                    except ValueError:
-                        print("That wasn't an integer... Please try again.")
-                        print("")
-                    while True:
-                        if z <= 0:
-                            print("")
-                            print("You can't have less than 1 attempt... Try again.")
-                            print("")
-                            break
-                        elif z > 0:
-                            break
-                    if z > 0:
-                        break
-                while True:
-                    print("")
-                    print(f"Scenario: You're guessing a random number from {x} to {y} and you have {z} attempt(s) to do so.")
-                    print("")
-                    redo = input("Would you like to reset your scenario? Y/N: ")
-                    if redo == "N" or redo == "n":
-                        print("")
-                        print("Good luck!")
-                        print("")
-                        break
-                    else:
-                        print("")
-                        break
-                if redo == "N" or redo == "n":
-                    break
+        x, y, z = get_range_and_attempts()
+        rng_game = random.randint(x, y)
+        attempts = 0
 
-        if attempts == 0 and invalid_attempts == 0:
-            rng_game = random.randint(x, y)
-        while guess is None:
-                try:
-                    guess = int(input("Guess the number: "))
-                except ValueError:
-                    invalid_attempts += 1
-                    print(f"That wasn't an integer... Your guess must be an integer between 1 and 100. Don't worry, you still have {z - attempts} attempts remaining.")
-                    print("")
-        if attempts == z and response != 'Y' and response != 'y':
+        while attempts < z:
+            guess = get_guess(x, y, attempts, z)
+            if guess is None:
+                continue
+
+            if guess < rng_game:
+                print("Your guess was too low...")
+            elif guess > rng_game:
+                print("Your guess was too high...")
+            else:
+                print("You did it! You guessed the number!")
+                break
+
+            attempts += 1
+            print(f"You have {z - attempts} attempt(s) remaining.")
+
+        if attempts == z:
+            print(f"Game over! You've exhausted your attempts. The number was {rng_game}.")
+
+        response = input("Do you want to play again? (Y/N): ")
+        if response.lower() != 'y':
             break
 
-        if guess > rng_game:
-            if guess > y:
-                invalid_attempts += 1
-                print(f"That guess is higher than {y}!... Remember, your guess must be between {x} and {y}. Don't worry, you still have {z - attempts} attempts remaining.")
-                print("")
-                guess = None
+        print()
+
+def get_range_and_attempts():
+    while True:
+        try:
+            x = int(input("Enter the minimum value of your range: "))
+            y = int(input("Enter the maximum value of your range: "))
+            z = int(input("Enter the number of attempts you would like to have: "))
+
+            if x >= y:
+                print("Invalid range! The minimum value should be smaller than the maximum value.")
+            elif z <= 0:
+                print("Invalid number of attempts! You must have at least 1 attempt.")
             else:
-                attempts += 1
-                print(f"Your guess was too high... You have {z - attempts} attempts remaining.")
-                print("")
-                guess = None
-        elif guess < rng_game:
-            if guess < x:
-                invalid_attempts += 1
-                print(f"That guess is lower than {x}!... Remember, your guess must be between {x} and {y}. Don't worry, you still have {z - attempts} attempts remaining.")
-                print("")
-                guess = None
+                return x, y, z
+
+        except ValueError:
+            print("Invalid input! Please enter integers for range and attempts.")
+
+def get_guess(x, y, attempts, max_attempts):
+    while True:
+        try:
+            guess = int(input("Guess the number: "))
+            if guess < x or guess > y:
+                print(f"Your guess should be between {x} and {y}.")
             else:
-                attempts += 1
-                print(f"Your guess was too low... You have {z - attempts} attempts remaining.")
-                print("")
-                guess = None
-        elif guess == rng_game:
-            print("You did it! You're a natural!!")
-            print("")
-            guess = None
-            response = input("Wanna do it again? Y/N: ")
-            print("")
-            invalid_attempts = 0
-            x = None
-            y = None
-            z = None
-            if response == 'Y' or response == 'y':
-                attempts = 0
-            else:
-                break
+                return guess
+
+        except ValueError:
+            print("Invalid input! Please enter an integer.")
+
+        attempts += 1
+        print(f"Don't worry, you still have {max_attempts - attempts} attempt(s) remaining.")
+
+    return None
+
 number_guess()
